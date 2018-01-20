bc66947e73cedf7a9c93024d93d2e6f96ce6bc86
Yang Yongli <yongli.yang@dianping.com>
2018-01-20 09:02:27 +0800

    design

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ LOG_FILE=${WORK_DIR}/app-${DATE}.log
 WAR_NAME="xiaobaolife-0.0.1-SNAPSHOT.war"
 
 echo "build a runnable package..."
-#./gradlew -Pprod bootRepackage
+./gradlew -Pprod bootRepackage
 
 echo "scp package to remote server"
 sshpass -p justdoit scp ./build/libs/xiaobaolife-0.0.1-SNAPSHOT.war  yongli@203.76.208.13:~/xiaobao/
